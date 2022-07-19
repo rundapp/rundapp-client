bc998ff38d8b55a8fb4b465153c771de719bbd9b
ernesto1034 <ernesto1034@yahoo.com>
2022-07-19 01:12:53 -0500

    changed BrowserRouter to HashRouter because of IPFS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // Installed Libraries
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 // Local Imports
@@ -12,8 +12,8 @@ const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 root.render(
 	<Provider store={store}>
-		<BrowserRouter>
+		<HashRouter>
 			<App />
-		</BrowserRouter>
+		</HashRouter>
 	</Provider>
 );
