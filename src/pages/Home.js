6f3a9d9ba5c37b497cb5f2ce35b6e746ab496aed
ernesto1034 <ernesto1034@yahoo.com>
2022-08-08 17:06:18 -0500

    moved faq panels to utils

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,6 +8,7 @@ import { ethers } from "ethers";
 import "../styles/Home.css";
 import ChallengeCard from "../components/ChallengeCard";
 import { secsToDate } from "../utils/Utils";
+import { faqPanels } from "../utils/FaqPanels";
 import RunChallenger from "../utils/RunChallenger.json";
 
 const Home = ({ windowWidth }) => {
@@ -40,43 +41,6 @@ const Home = ({ windowWidth }) => {
 		const newIndex = activeFaqIndex == index ? -1 : index;
 		setActiveFaqIndex(newIndex);
 	};
-
-	// FAQ Rows
-	const faqPanels = [
-		{
-			key: "q1",
-			title: "Where does RunDapp store users' money?",
-			content: "RunDapp stores users' money in a smart contract account.",
-		},
-		{
-			key: "q2",
-			title: "Is RunDapp free to use?",
-			content:
-				"No. Currently, a 4% service fee and applicable transaction gas fees are deducted from every challenge bounty.",
-		},
-		{
-			key: "q3",
-			title: "What networks do you support",
-			content:
-				"Currently, we only support Polygon Mainnet, but we intend to expand to other networks soon.",
-		},
-		{
-			key: "q4",
-			title: "How do I record a run on Strava?",
-			content: "<LINK TO SUPPORT ARTICLE.>",
-		},
-		{
-			key: "q5",
-			title: "Can I record my run with a wearable device (Apple, Garmin, Fitbit, Samsung, etc.)?",
-			content:
-				"Yes. <LINK TO ARTICLE LISTING STRAVA-COMPATIBLE WEARABLES>",
-		},
-		{
-			key: "q6",
-			title: "How do I upload run to Strava from my wearable device?",
-			content: "<LINK TO SUPPORT ARTICLE.>",
-		},
-	];
 
 	useEffect(() => {
 		getChallengesArray();
